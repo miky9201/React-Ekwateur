9e6a640e0ec644c4ed36bee9bd7f12bf4e50bc7c
Mike Baldacci <baldaccimike@gmail.com>
2021-04-14 15:18:59 +0200

    Commit clé : j'ai récupérer les données de conso

--- src/components/EnergyConsumption.tsx
+++ src/components/EnergyConsumption.tsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
 const EnergyConsumption = () => {
-      const [ gasConsumption, setGasConsumption ] = useState([])
+
+      const [ gasConsumption, setGasConsumption ] = useState<gasConsumptionType[]>([]);
 
       useEffect(() => {
             async function getGasConsumption() {
                   const response = await fetch('https://5e9ed3cdfb467500166c47bb.mockapi.io/api/v1/meter/1/gas');
                   const data = await response.json();
-                  const emptyArr: any = [];
+                  const emptyArr: any[] = []
                   for (let index = 0; index < data.length; index++) {
-                        const gasConsumptionLine = {
+                        const gasConsumptionLine: object = {
                               date: data[index].createdAt.substr(0, 10),
                               time: data[index].createdAt.substr(11, 8),
                               indexHigh: data[index].indexHigh
@@ -21,11 +22,19 @@ const EnergyConsumption = () => {
             getGasConsumption()
       }, [])
 
+      interface gasConsumptionType {
+            date: string;
+            time: string;
+            indexHigh : number;
+      }
+
       return (
             <div className="meters-data-div white">
-                  {gasConsumption.map(line => <p>Date du relevé : {line} </p>)}
+                  <ul>
+                        {gasConsumption.map( item => <li>Le {item.date}, à {item.time}, vous avez consommé {item.indexHigh} kwH de Gaz</li>)}
+                  </ul>
             </div>
-            )
+      )
 }
 
 export default EnergyConsumption;
--- src/components/MeterDataDisplayer.tsx
+++ src/components/MeterDataDisplayer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const MeterDataDisplayer = ({ meterTime, meterDate, energyType, label } : { meterTime: string, meterDate: string, energyType: string, label: string }) => {
-
       return (
             <div className="meters-data-div green">
                   <p>{label} POD : {energyType}</p>
