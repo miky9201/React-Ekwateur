986abd8039c13e56abe2d47b5eb6c21522c3d591
Mike Baldacci <baldaccimike@gmail.com>
2021-04-14 10:22:15 +0200

    Commit pour commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import React, { useState, useEffect} from 'react';
 import './App.css';
-import { fetchDataFromMeters } from './api-calls/fetchDataFromMeters';
+import MeterDataDisplayer from './components/MeterDataDisplayer';
 
 function App() {
 
-      const [gasPod, setGasPod] = useState(null)
-
-      // const MetersDataDisplayer = () => (
-      //       <div className="meters-data-div white">
-      //             <p>POD Gaz : {}</p>
-      //       </div>
-      // )
+      const [energyPod, setEnergyPod] = useState({})
 
       const GAS_ID = 1;
       const ELECTRICITY_ID = 2;
 
       useEffect(() => {
-            async function anyNameFunction() {
+
+            async function getEnergyPods() {
                   const response = await fetch('https://5e9ed3cdfb467500166c47bb.mockapi.io/api/v1/meter/');
                   const data = await response.json();
-                  const item = data[GAS_ID - 1].pointOfDelivery;
-                  setGasPod(item);
+                  const item = {
+                        gasPod: data[GAS_ID - 1].pointOfDelivery,
+                        electricityPod: data[ELECTRICITY_ID - 1].pointOfDelivery
+                  }
+                  setEnergyPod(item);
             }
-            anyNameFunction()
+
+            getEnergyPods()
       }, [])
       
 
@@ -30,10 +29,7 @@ function App() {
             <section className="app-container">
                   <h1>Bonjour, Voici votre consommation : </h1>
                   <div className="meters-data-container">
-                        {/* <MetersDataDisplayer /> */}
-                        <div className="meters-data-div white">
-                              <p>POD Gaz : {gasPod}</p>
-                        </div>
+                        <MeterDataDisplayer energyPod={energyPod}/>
                   </div>
             </section>
       )
--- src/api-calls/fetchDataFromMeters.ts
+++
@@ -1,5 +0,0 @@
-export const fetchDataFromMeters = async (meterId: number) => {
-      await fetch('https://5e9ed3cdfb467500166c47bb.mockapi.io/api/v1/meter/')
-      .then(res => res.json())
-      .then( data => data[meterId - 1].pointOfDelivery)
-}
